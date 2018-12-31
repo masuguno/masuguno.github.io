8bb9ead0fdead0cef2b619cf6d9c9e94f84dc3be
Nguyen Tuan Duong <hoangchiduong49@gmail.com>
2018-12-31 22:05:01 +0700

    ok

--- date/DailyRewardTIme.php
+++ date/DailyRewardTIme.php
@@ -1,6 +1,6 @@
 <?php
  date_default_timezone_set('US/Eastern');
  $currenttime = date("m-d-Y H:i:s");
- list($ddd,$ttt) = split(' ',$currenttime);
+ list($ddd,$ttt) = explode(' ',$currenttime);
  echo "$ddd/$ttt\n";
 ?>
